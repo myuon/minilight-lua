0288bb6216aad1ece72b670823f364c7b54271f9
myuon <ioi.joi.koi.loi@gmail.com>
2020-05-10 16:56:41 +0900

    chore: publish script

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+if [ $# -ne 1 ]; then
+  echo "No version specified" 1>&2
+  exit 1
+fi
+cabal new-sdist
+
+dir=$(mktemp -d dist-docs.XXXXXX)
+trap 'rm -r "$dir"' EXIT
+
+cabal new-haddock --builddir="$dir" --haddock-for-hackage --enable-documentation --haddock-option=--hyperlinked-source && \
+cabal upload --publish dist-newstyle/sdist/minilight-lua-$1.tar.gz && \
+cabal upload --publish -d $dir/minilight-lua-$1-docs.tar.gz
